77b3f5eadbe882d3abd8f0fece1cee805504727a
MustafaK01 <mestefe119@gmail.com>
2023-07-04 03:12:45 +0300

    edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SearchDirectionsComponent } from './pages/search-directions/search-directions/search-directions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    SearchDirectionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
